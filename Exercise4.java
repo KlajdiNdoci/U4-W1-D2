f5991e74d7bb803c9ffd3a24821bfa2ad493f8ce
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-10-03 15:16:08 +0200

    exercise 4

---
+++ Exercise4.java
@@ -0,0 +1,10 @@
+public class Exercise4 {
+    public static void main(String[] args) {
+
+        int x = 100;
+
+        for (int i = x; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
