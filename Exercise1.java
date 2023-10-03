2d0d3cf069d465a73a92b0cd543771514c7fb892
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-10-03 14:38:26 +0200

    exercise 1

--- Exercise1.java
+++ Exercise1.java
@@ -1,5 +1,33 @@
 public class Exercise1 {
     public static void main(String[] args) {
-        int ciao = 2;
+
+        System.out.println(evenOdd("ciao"));
+        System.out.println(evenOdd("ciaoo"));
+
+        System.out.println(isLeapYear(1998));
+        System.out.println(isLeapYear(1996));
+
+    }
+
+    public static String evenOdd(String str) {
+        if (str.length() % 2 == 0) {
+            System.out.println("The number of letters of the string is even");
+        } else {
+            System.out.println("The number of letters of the string is odd");
+        }
+        return str;
+    }
+
+    public static boolean isLeapYear(int year) {
+        boolean leapYear = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        if (leapYear) {
+            System.out.println("the year " + year + " is a leap year");
+            return true;
+
+        } else {
+            System.out.println("the year " + year + " is not a leap year");
+            return false;
+        }
+        
     }
 }
